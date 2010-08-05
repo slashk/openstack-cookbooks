ee445330753097b9a1678adea36f843616aa6871
Darrin Eden <darrin.eden@gmail.com>
2010-08-06 03:15:09 +0800

    generic branch name

--- cookbooks/swift/attributes/default.rb
+++ cookbooks/swift/attributes/default.rb
@@ -2,6 +2,6 @@ default[:cloudfiles][:user] = "root"
 default[:cloudfiles][:group] = "root"
 default[:cloudfiles][:homedir] = "/root"
 default[:cloudfiles][:bzr_email] = "you@example.com"
-default[:cloudfiles][:bzr_branch] = "lp:~swift-core/swift/trunk"
+default[:cloudfiles][:bzr_branch] = "lp:swift"
 default[:cloudfiles][:launchpad_login] = "gimme"
   
