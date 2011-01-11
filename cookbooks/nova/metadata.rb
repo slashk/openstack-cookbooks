93e5d3776dd095d26c16e519b0856d8b1db277c3
Vishvananda Ishaya <vishvananda@gmail.com>
2011-01-10 19:01:25 -0800

    add apt dependency

--- cookbooks/nova/metadata.rb
+++ cookbooks/nova/metadata.rb
@@ -5,6 +5,7 @@ description      "Installs/Configures nova"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
+depends "apt"
 depends "openldap"
 depends "python-ldap"
 depends "build-essential"
