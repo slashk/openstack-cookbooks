c55c1b9e53d641a37e4aafffb2969b49751b20a8
Vishvananda Ishaya <vishvananda@gmail.com>
2011-01-16 14:54:02 -0800

    Import openssl version 0.1.0

--- cookbooks/nova/attributes/default.rb
+++ cookbooks/nova/attributes/default.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: nova
+# Attributes:: default
+#
+# Copyright 2008-2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 default[:nova][:install_type] = "binary"
 default[:nova][:compute_connection_type] = "qemu"
 default[:nova][:user] = "admin"
@@ -6,4 +25,3 @@ default[:nova][:creds][:user] = "nova"
 default[:nova][:creds][:group] = "nogroup"
 default[:nova][:creds][:dir] = "/var/lib/nova"
 default[:nova][:images] = []
-
