488c8643e78bf44f88c6353f6e4e11f622365473
Devin Carlen <devin.carlen@gmail.com>
2011-01-25 12:26:04 -0800

    Added apache and wsgi configuration

--- cookbooks/nova/attributes/dashboard.rb
+++ cookbooks/nova/attributes/dashboard.rb
@@ -1,5 +1,7 @@
-default[:nova][:dashboard][:deploy_dir] = "/deploy"
+default[:nova][:dashboard][:admin_email] = "vagrant@localhost"
+default[:nova][:dashboard][:deploy_dir] = "/srv"
 default[:nova][:dashboard][:dashboard_dir] = File.join(node[:nova][:dashboard][:deploy_dir], "openstack-dashboard")
 default[:nova][:dashboard][:dashboard_branch] = "lp:openstack-dashboard"
 default[:nova][:dashboard][:django_nova_dir] = File.join(node[:nova][:dashboard][:deploy_dir], "django-nova")
 default[:nova][:dashboard][:django_nova_branch] = "lp:django-nova"
+default[:nova][:dashboard][:apache_dir] = "/etc/apache2"
--- cookbooks/nova/recipes/dashboard.rb
+++ cookbooks/nova/recipes/dashboard.rb
@@ -1,10 +1,19 @@
 package "bzr"
+package "apache2"
+package "libapache2-mod-wsgi"
 
 directory "#{node[:nova][:dashboard][:deploy_dir]}" do
   owner "root"
   group "root"
   mode 0755
   action :create
+end
+
+directory "#{node[:nova][:dashboard][:deploy_dir]}" do
+    owner "root"
+    group "root"
+    mode 0755
+    action :create
 end
 
 execute "bzr branch #{node[:nova][:dashboard][:django_nova_branch]} #{node[:nova][:dashboard][:django_nova_dir]}" do
@@ -16,7 +25,33 @@ execute "bzr branch #{node[:nova][:dashboard][:dashboard_branch]} #{node[:nova][
     cwd node[:nova][:dashboard][:deploy_dir]
 end
 
+execute "python setup.py install" do
+    cwd node[:nova][:dashboard][:django_nova_dir]
+end
 
+execute "python tools/install_venv.py" do
+    cwd node[:nova][:dashboard][:dashboard_dir]
+end
 
+template "#{node[:nova][:dashboard][:dashboard_dir]}/local/local_settings.py" do
+    owner "root"
+    group "root"
+    source "dashboard.local_settings.erb"
+    mode "0644"
+end
 
+template "#{node[:nova][:dashboard][:dashboard_dir]}/local/dashboard.wsgi" do
+    owner "root"
+    group "root"
+    source "dashboard.wsgi.erb"
+end
+
+template "#{node[:nova][:dashboard][:apache_dir]}/sites-available/default" do
+  owner "root"
+  group "root"
+  source "dashboard.apache.erb"
+  mode "0644"
+end
+
+execute "/etc/init.d/apache2 restart"
 
