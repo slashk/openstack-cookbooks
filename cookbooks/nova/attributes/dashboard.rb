9dab4ba02fcdb4b4ae9780101ae29d5acec56bd3
Devin Carlen <devin.carlen@gmail.com>
2011-01-26 15:56:50 -0800

    Renamed admin user

--- cookbooks/nova/attributes/dashboard.rb
+++ cookbooks/nova/attributes/dashboard.rb
@@ -1,8 +1,9 @@
-default[:nova][:dashboard][:admin_email] = "vagrant@example.com"
-default[:nova][:dashboard][:admin_username] = "vagrant"
+default[:nova][:dashboard][:admin_email] = "admin@example.com"
+default[:nova][:dashboard][:admin_username] = "admin"
 default[:nova][:dashboard][:deploy_dir] = "/srv"
 default[:nova][:dashboard][:dashboard_dir] = File.join(node[:nova][:dashboard][:deploy_dir], "openstack-dashboard")
 default[:nova][:dashboard][:dashboard_branch] = "lp:openstack-dashboard"
 default[:nova][:dashboard][:django_nova_dir] = File.join(node[:nova][:dashboard][:deploy_dir], "django-nova")
 default[:nova][:dashboard][:django_nova_branch] = "lp:django-nova"
 default[:nova][:dashboard][:apache_dir] = "/etc/apache2"
+
