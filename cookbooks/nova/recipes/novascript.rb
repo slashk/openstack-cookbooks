29f7b889598ac9ab09cde2a53f7cbbc765ed78f1
Vishvananda Ishaya <vishvananda@gmail.com>
2011-03-07 19:00:04 -0800

    add environment to nova.sh

--- cookbooks/nova/recipes/novascript.rb
+++ cookbooks/nova/recipes/novascript.rb
@@ -29,6 +29,10 @@ end
 
 execute "./novascript/nova.sh run_detached" do
   user "root"
+  environment ({INTERFACE => node[:nova][:vlan_interface],
+                FLOATING_RANGE => node[:nova][:floating_range],
+                FIXED_RANGE => node[:nova][:floating_range],
+                HOST_IP => node[:nova][:my_ip]})
 end
 
 execute "unzip -o ./nova/nova.zip -d #{node[:nova][:creds][:dir]}/" do
