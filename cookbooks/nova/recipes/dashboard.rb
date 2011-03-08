0ee0cfc4e2ffae3c70ff4e844f61fde45cf57237
termie <github@anarkystic.com>
2011-03-08 15:32:40 -0800

    remove branching of django-nova from dashboard

--- cookbooks/nova/recipes/dashboard.rb
+++ cookbooks/nova/recipes/dashboard.rb
@@ -18,11 +18,6 @@ directory "#{node[:nova][:dashboard][:deploy_dir]}" do
     action :create
 end
 
-execute "bzr branch #{node[:nova][:dashboard][:django_nova_branch]} #{node[:nova][:dashboard][:django_nova_dir]}" do
-    cwd node[:nova][:dashboard][:deploy_dir]
-    not_if { File.directory?(node[:nova][:dashboard][:django_nova_dir]) }
-end
-
 execute "bzr branch #{node[:nova][:dashboard][:dashboard_branch]} #{node[:nova][:dashboard][:dashboard_dir]}" do
     cwd node[:nova][:dashboard][:deploy_dir]
 end
